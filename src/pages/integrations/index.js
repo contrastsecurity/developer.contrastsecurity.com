ad341040c4522ca75f518a8a948b58dd2aeea49b
Erik Costlow <erikcostlow@outlook.com>
2021-10-08 14:40:23 -0400

    Integrations page

--- src/pages/integrations/index.js
+++ src/pages/integrations/index.js
@@ -27,23 +27,20 @@ const IntegrationItems = [
       integrations: [
         {
             title: 'Cloud Foundry',
-            Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-            links: [{text: 'MyText', to: '/somewhere'}, {text: 'Second', to: '/elsewhere'}]
+            links: [{text: 'For Node.js', to: 'https://docs.contrastsecurity.com/installation-nodeintegrations.html#cloud-foundry'}, 
+                    {text: 'For Java', to: 'https://docs.contrastsecurity.com/en/vmware-tanzu-network-for-java.html'}]
         },
         {
             title: 'Microsoft Azure',
-            Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-            links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
+            links: [{text: 'For .NET', to: 'https://docs.contrastsecurity.com/installation-netusage.html#azure'}, {text: 'For .NET Core', to: 'https://docs.contrastsecurity.com/installation-netcoreusage.html#netcore-usage-azure'}]
         },
         {
             title: 'Google App Engine',
-            Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-            links: [{text: 'For Java', to: '/somewhere'}]
+            links: [{text: 'For Java', to: 'https://docs.contrastsecurity.com/en/google-app-engine.html'}]
         },
         {
             title: 'Elastic Beanstalk',
-            Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-            links: [{text: 'For Java', to: '/somewhere'}]
+            links: [{text: 'For Java', to: 'https://docs.contrastsecurity.com/en/aws-elastic-beanstalk.html'}]
         }
       ]
     },
@@ -54,12 +51,10 @@ const IntegrationItems = [
         integrations: [
             {
                 title: 'Microsoft Teams',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
                 links: [{text: 'MyText', to: '/somewhere'}, {text: 'Second', to: '/elsewhere'}]
             },
             {
                 title: 'Slack',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
                 links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
             }
         ]
@@ -71,42 +66,210 @@ const IntegrationItems = [
         integrations: [
             {
                 title: 'Jenkins',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-                links: [{text: 'MyText', to: '/somewhere'}, {text: 'Second', to: '/elsewhere'}]
+                links: [{text: 'Jenkins Plugin', to: 'https://plugins.jenkins.io/contrast-continuous-application-security'}, {text: 'Source code', to: 'https://github.com/jenkinsci/contrast-continuous-application-security-plugin'},
+                  {text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/jenkins.html'}]
             },
             {
                 title: 'Azure DevOps',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-                links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
+                links: [{text: 'Azure Marketplace', to: 'https://marketplace.visualstudio.com/items?itemName=contrast-security.contrast-integration'}, {text: 'Documentation', to: 'https://marketplace.visualstudio.com/items?itemName=contrast-security.contrast-integration'}]
             },
             {
                 title: 'Circle CI',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-                links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
+                links: [{text: 'Orb Registry', to: 'https://circleci.com/orbs/registry/orb/contrastsecurity/verify'}, {text: 'Source Code', to: 'https://github.com/Contrast-Security-OSS/contrast-security-orb'}]
             },
             {
                 title: 'Bamboo',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-                links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
+                links: [{text: 'Atlassian Marketplace', to: 'https://marketplace.atlassian.com/apps/1219111/contrast-security-for-bamboo?hosting=server&tab=overview'},
+                  {text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/contrast-bamboo-plugin'},
+                  {text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/bamboo.html'}]
             },
             {
                 title: 'Maven',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-                links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
+                links: [{text: 'Maven Central', to: 'https://search.maven.org/search?q=a:contrast-maven-plugin'},
+                        {text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/contrast-maven-plugin'},
+                        {text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/maven.html'},
+                        {text: 'Sample Project', to: 'https://github.com/Contrast-Security-OSS/vulnerable-spring-boot-application'}
+                      ]
             },
             {
                 title: 'Gradle',
-                Svg: require('../../../static/img/undraw_docusaurus_mountain.svg').default,
-                links: [{text: 'For .NET', to: '/somewhere'}, {text: 'For .NET Core', to: '/elsewhere'}]
+                links: [{text: 'gradle.org', to: 'https://plugins.gradle.org/plugin/com.contrastsecurity.contrastplugin'}, {text: 'Source Code', to: 'https://github.com/Contrast-Security-OSS/contrast-gradle-plugin'},
+                  {text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/gradle.html'}, {text: 'Sample Project', to: 'https://github.com/Contrast-Security-OSS/Contrast-Sample-Gradle-Application'}]
             }
         ]
     },
+    {
+      title: 'Work Tracking Platforms',
+      description: (<>Ensure that application vulnerabilities will get remediated by having tickets created for resolving each security issue.</>
+      ),
+      integrations: [
+          {
+              title: 'GitHub',
+              links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/github.html'}]
+          },
+          {
+            title: 'Azure Boards',
+            links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/azure-boards.html'}]
+          },
+          {
+            title: 'Jira',
+            links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/jira-integration.html'}]
+          },
+          {
+            title: 'CA Agile Central',
+            links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/agile-central.html'}]
+          },
+          {
+            title: 'Bugzilla',
+            links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/bugzilla.html'}]
+          },
+          {
+            title: 'Serena',
+            links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/serena-business-manager.html'}]
+          }
+      ]
+    },
+    {
+      title: 'Work Tracking Platforms',
+      description: (<>Learn about your applications' vulnerabilities and receive remediation guidance while in your favorite development environment.</>
+      ),
+      integrations: [
+        {
+            title: 'Visual Studio Code',
+            links: [{text: 'Marketplace', to: 'https://marketplace.visualstudio.com/items?itemName=ContrastSecurity.contrastsecurity'}]
+        },
+        {
+          title: 'Visual Studio',
+          links: [{text: 'Marketplace', to: 'https://marketplace.visualstudio.com/items?itemName=contrast-security.contrast-vs-ext'}]
+        },
+        {
+          title: 'Visual Studio for Mac',
+          links: [{text: 'Extension file', to: 'https://github.com/Contrast-Security-OSS/contrast-vsmac-plugin-distro/releases'}]
+        },
+        {
+          title: 'IntelliJ',
+          links: [{text: 'Marketplace', to: 'https://plugins.jetbrains.com/plugin/10335-contrast'}]
+        },
+        {
+          title: 'Eclipse',
+          links: [{text: 'Marketplace', to: 'https://marketplace.eclipse.org/content/contrast-eclipse'}, {text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/ide-plugin-eclipse2'}]
+        },
+      ]
+    },
+    {
+      title: 'Incident Management Systems',
+      description: (<>Be confident that on-call personnel will be equipped to take necessary action against attacks on your application.</>
+      ),
+      integrations: [
+        {
+            title: 'PagerDuty',
+            links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/pagerduty.html'}]
+        },
+        {
+          title: 'VictorOps',
+          links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/victorops.html'}]
+      },
+      ]
+    },
+    {
+      title: 'Security Information and Event Management (SIEM) Tools',
+      description: (<>Receive directly in your SIEM operational insights into application security threats from Contrast's instrumentation activities.</>
+      ),
+      integrations: [
+        {
+            title: 'Splunk',
+            links: [{text: 'Marketplace', to: 'https://splunkbase.splunk.com/app/4140/'}, {text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/ContrastSplunkApp'}]
+        },
+        {
+          title: 'Sumo Logic',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/ContrastSumoLogicDashboards'}]
+        },
+        {
+          title: 'DataDog',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/ContrastDataDogDashboard'}]
+        },
+      ]
+    },
+    {
+      title: 'Extend the Contrast Platform',
+      description: (<>Use Contrast's SDKs and Webhooks to build custom services and/or notify them upon the discovery of new vulnerabilities or attacks using Webhooks.</>
+      ),
+      integrations: [
+        {
+            title: 'Java SDK',
+            links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/contrast-sdk-java'}]
+        },
+        {
+          title: 'JavaScript SDK',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/contrast-sdk-javascript'}, {text: 'NPM', to: 'https://www.npmjs.com/package/contrast-sdk'}]
+        },
+        {
+          title: 'Python SDK',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/contrast-sdk-python'}]
+        },
+        {
+          title: '.NET SDK',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/contrast-sdk-dotnet'}, {text: 'NuGet', to: 'https://www.nuget.org/packages/ContrastRestClient/'}]
+        },
+        {
+          title: 'Webhook',
+          links: [{text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/generic-webhooks.html'}]
+        },
+        {
+          title: 'Contrast CLI',
+          links: [{text: 'NPM', to: 'https://www.npmjs.com/package/@contrast/contrast-cli'}, {text: 'Documentation', to: 'https://docs.contrastsecurity.com/en/contrast-cli.html'}]
+        },
+      ]
+    },
+    {
+      title: 'Vulnerability Aggregators',
+      description: (<>Vulnerability management platforms with which Contrast is compatible.</>
+      ),
+      integrations: [
+        {
+            title: 'ThreadFix',
+            links: [{text: 'Contrast Integration', to: 'https://threadfix.it/integrations/?_sft_scanner=interactive-iast'}]
+        },
+        {
+          title: 'CodeDx',
+          links: [{text: 'Contrast Integration', to: 'https://codedx.com/blog/whats-coming-in-code-dx-2-4/'}]
+      },
+      ]
+    },
+    {
+      title: 'Testing',
+      description: (<>Tools that Contrast uses to test applications.</>
+      ),
+      integrations: [
+        {
+            title: 'Sheepdog',
+            links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/sheepdog'}]
+        },
+        {
+          title: 'Node Test Bench Apps',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/NodeTestBenches'}]
+        },
+        {
+          title: 'Hapi 16 Test Bench',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/Hapi16TestBench'}]
+        },
+        {
+          title: 'Node Dvnr',
+          links: [{text: 'Source code', to: 'https://github.com/Contrast-Security-OSS/node-dvnr'}]
+        },
+      ]
+    }
 ]
 
-function IntegrationItem({title, Svg, links}) {
+function makeImageName(name){
+  return require('./' + name.replace(/\W/g, '').toLowerCase() + '.png').default
+}
+
+function IntegrationItem({title, links}) {
     return (
-      <div className={clsx('col col--4', styles.integration)}>
+      <div className={clsx('col', styles.integration)}>
         <h4>{title}</h4>
+        <img src={makeImageName(title)} alt="image" className={clsx(styles.logo)} />
         <div className={clsx('links')}>
             {links.map((props, idx) => (
                 <><a href={props.to}>{props.text}</a><br/></>
