26fd6a9504176fdd14d21a56fd853508966f48c0
Erik Costlow <erikcostlow@outlook.com>
2021-11-05 16:50:00 -0400

    Initial (incomplete) search capabilities

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/getting-started/where-do-i-start',
               },
             ],
           },
