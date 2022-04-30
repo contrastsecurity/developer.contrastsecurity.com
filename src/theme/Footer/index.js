bb95cdc651342e22f25081b69d4a7a379f89df87
Joshua Granick <jgranick.users.noreply@github.com>
2022-04-29 18:20:34 -0700

    Add HS

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <Footer {...props} />
+      <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/203759.js"></script>
+    </>
+  );
+}
